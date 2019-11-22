18500f92f1946a3961a85823170f3b67ba598140
Prajwal <prajwal.shetty@sas.com>
2019-11-22 14:13:04 +1100

    Adding a check to make sure --redeploy fails when the job was not found in metadata.

--- runSAS.sh
+++ runSAS.sh
@@ -6,7 +6,7 @@
 #                                                                                                                    #
 #        Desc: The script can run and monitor SAS Data Integration Studio jobs.                                      #
 #                                                                                                                    #
-#     Version: 14.7                                                                                                  #
+#     Version: 14.8                                                                                                  #
 #                                                                                                                    #
 #        Date: 03/11/2019                                                                                            #
 #                                                                                                                    #
@@ -100,7 +100,7 @@ function display_welcome_ascii_banner(){
 printf "\n${green}"
 cat << "EOF"
 +-+-+-+-+-+-+ +-+-+-+-+-+
-|r|u|n|S|A|S| |v|1|4|.|7|
+|r|u|n|S|A|S| |v|1|4|.|8|
 +-+-+-+-+-+-+ +-+-+-+-+-+
 |P|r|a|j|w|a|l|S|D|
 +-+-+-+-+-+-+-+-+-+
@@ -115,7 +115,7 @@ printf "\n${white}"
 #------
 function show_the_script_version_number(){
 	# Version numbers
-	RUNSAS_CURRENT_VERSION=14.7                                      
+	RUNSAS_CURRENT_VERSION=14.8                                      
 	RUNSAS_IN_PLACE_UPDATE_COMPATIBLE_VERSION=12.2
     # Show version numbers
     if [[ ${#@} -ne 0 ]] && ([[ "${@#"--version"}" = "" ]] || [[ "${@#"-v"}" = "" ]] || [[ "${@#"--v"}" = "" ]]); then
@@ -1991,6 +1991,10 @@ function redeploy_sas_jobs(){
 															
 				# Fix the file name with underscores (SAS DI currently does this by default, just to keep it in-sync with deployed job name referred in .jobs.list file)
 				deployed_job_sas_file=$depjob_sourcedir/${job##/*/}.sas
+                
+                # A way to check if the job was deployed at all?
+                check_if_the_file_exists $deployed_job_sas_file
+
 				mv "$deployed_job_sas_file" "${deployed_job_sas_file// /_}"
 
                 # Add it to audit log
